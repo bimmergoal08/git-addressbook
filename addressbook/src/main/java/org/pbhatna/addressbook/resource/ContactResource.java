0bf8c56e7c69e0f71763ca6ad6e31ad4a40ab42d
priyank bhatnagar <priyankb08@gmail.com>
2015-10-18 17:00:27 -0500

    Ver2

--- addressbook/src/main/java/org/pbhatna/addressbook/resource/ContactResource.java
+++ addressbook/src/main/java/org/pbhatna/addressbook/resource/ContactResource.java
@@ -1,5 +1,10 @@
 package org.pbhatna.addressbook.resource;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -12,8 +17,10 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.pbhatna.addressbook.database.DatabaseConnection;
 import org.pbhatna.addressbook.model.Contact;
 import org.pbhatna.addressbook.service.ContactService;
+
 
 @Path("/contacts")
 @Produces(MediaType.APPLICATION_JSON)
@@ -21,7 +28,7 @@ import org.pbhatna.addressbook.service.ContactService;
 public class ContactResource {
 	
 	ContactService contactService = new ContactService();
-
+	
 	@GET
 	public List<Contact> getContacts() {
 		return contactService.getAllContacts();
@@ -51,4 +58,20 @@ public class ContactResource {
 		return contactService.getContact(contactId);
 	}
 	
+//	@GET
+//	@Path("/database")
+//	@Produces(MediaType.TEXT_HTML)
+//	public String getConnectionStatus() {
+//		String str = "";
+//		try {
+//			
+//			str = DatabaseConnection.getDateTime();
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return str;
+//	}
+		
+		
 }
